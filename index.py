872b8e3012a0c8e23490e5d2087dd67aea42f664
linuxing <li.dinglong@163.com>
2016-02-02 14:36:02 +0800

    database migration

--- index.py
+++ index.py
@@ -1,34 +1,74 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import session, redirect,url_for,flash
 
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
+from flask.ext.script import Shell
+from flask.ext.migrate import Migrate,MigrateCommand
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 class NameForm(Form):
 	name = StringField("What is your name?", validators=[Required()] )
 	submt = SubmitField('submt')
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
-# manager = Manager(app)
-bootstrap = Bootstrap(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+def make_shell_context():
+	return dict(app=app,db=db,User=User,Role=Role)
+
+manager.add_command("shell",Shell(make_context=make_shell_context))
 
 @app.route('/',methods=['GET','Post'])
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('looks like you have changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
-	return render_template('index.html',form = form,name = session.get('name') )
+	return render_template('index.html',form = form, name = session.get('name'),
+		known = session.get('known', False) )
 
 @app.route('/user/<name>')
 def user(name):
@@ -44,5 +84,4 @@ def internal_err(e):
 
 
 if __name__ == '__main__':
-	# manager.run()
-	app.run(debug=True)
+	manager.run()
