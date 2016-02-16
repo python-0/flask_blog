86efd3d0ab429c56f00642b7cba7793705f771dc
linuxing <li.dinglong@163.com>
2016-02-16 22:31:29 +0800

    confirm account

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from .. import db
+from ..email import send_mail
 from .forms import LoginForm, RegistrationForm
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from ..models import User
 
 
@@ -32,9 +33,50 @@ def register():
 	if form.validate_on_submit():
 		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
 		db.session.add(user)
-		flash('You can now login')
-		return redirect(url_for('auth.login'))
+		db.session.commit()
+		token = user.generate_confirmation_token()
+		send_mail(user.email, 'Confirm Your Account',
+		              'auth/email/confirm', user=user, token=token)
+		flash('A confirmation email has been sent to you by email')
+		return redirect(url_for('main.index'))
 	return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+	if current_user.confirmed:
+		return redirect(url_for('main.index'))
+	if current_user.confirm(token):
+		flash('You have confirmed your account, Thanks')
+	else:
+		flash('The confirmation link is invalid or has expired')
+	return redirect('main.index')
+
+
+@auth.before_app_request
+def before_request():
+	if current_user.is_authenticated() \
+			and not current_user.confirmed() \
+			and request.endpoint[:5] != 'auth.' \
+			and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/auth.unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous() or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+	token = current_user.generate_confirmation_token()
+	send_mail(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
+	flash('A new confirmation email has been sent to you by email')
+	return redirect(url_for('main.index'))
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from . import db
 from flask_login import UserMixin
 from . import loginManager
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
 @loginManager.user_loader
@@ -21,11 +23,29 @@ class Role(db.Model):
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
-    id = db.Column( db.Integer, primary_key=True )
-    email = db.Column( db.String(64),unique=True,index=True )
-    username = db.Column( db.String(64), unique=True, index=True )
-    password_hash = db.Column( db.String(128) )
-    role_id = db.Column( db.Integer, db.ForeignKey('roles.id') )
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64),unique=True,index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer.dumps(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmd = True
+        db.session.add(self)
+        return True
 
     @property
     def password(self):
